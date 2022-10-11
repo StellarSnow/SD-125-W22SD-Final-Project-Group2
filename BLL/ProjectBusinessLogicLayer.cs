a0591897a3435621faae9fb86dd894c577235689
Dane Dwarka <danedwarka@hotmail.com>
2022-10-11 13:37:06 -0500

    Add ProjectBusinessLogicLayer

---
+++ BLL/ProjectBusinessLogicLayer.cs
@@ -0,0 +1,19 @@
+﻿using SD_340_W22SD_Final_Project_Group6.DAL;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL;
+
+public class ProjectBusinessLogicLayer
+{
+    private readonly IRepository<Project> _projectRepository;
+
+    public ProjectBusinessLogicLayer(IRepository<Project> projectRepository)
+    {
+        _projectRepository = projectRepository;
+    }
+
+    public List<Project> GetAll()
+    {
+        return _projectRepository.GetAll().ToList();
+    }
+}
