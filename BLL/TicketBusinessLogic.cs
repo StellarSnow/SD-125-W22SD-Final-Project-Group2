df9122a4e92abfc718fd62873d39cfd648fb9125
Dane Dwarka <danedwarka@hotmail.com>
2022-10-12 20:01:26 -0500

    Remove Save() in TicketBusinessLogic so that it just has SaveTicket()

--- BLL/TicketBusinessLogic.cs
+++ BLL/TicketBusinessLogic.cs
@@ -27,11 +27,6 @@ namespace SD_340_W22SD_Final_Project_Group6.BLL
             return _repo.Update(ticket);
         }
 
-        public void Save()
-        {
-            _repo.Save();
-        }
-
         public void SaveTicket()
         {
             _repo.Save();
