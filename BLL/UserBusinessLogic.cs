1f19882cd4a1702098a6d6602cdc9ac6c0d4021a
Dane Dwarka <danedwarka@hotmail.com>
2022-10-13 17:44:46 -0500

    Add UserBusinessLogic Class

---
+++ BLL/UserBusinessLogic.cs
@@ -0,0 +1,20 @@
+﻿using SD_340_W22SD_Final_Project_Group6.DAL;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL
+{
+    public class UserBusinessLogic
+    {
+        private IRepository<ApplicationUser> _userRepository { get; set; }
+
+        public UserBusinessLogic(IRepository<ApplicationUser> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public ApplicationUser GetUser(string id)
+        {
+            return _userRepository.Get(id);
+        }
+    }
+}
