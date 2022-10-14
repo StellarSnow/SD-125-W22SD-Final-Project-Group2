62fb83cae4da3cef9845ca03136ebaf5e5e06454
asad <asadtahir9999@gmail.com>
2022-10-13 22:23:22 -0500

    added DAL for comments and watchers

---
+++ DAL/CommentRepository.cs
@@ -0,0 +1,75 @@
+﻿using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.DAL
+{
+    public class CommentRepository : IRepository<Comment>
+    {
+        public ApplicationDbContext DbContext { get; set; }
+
+        public CommentRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public void Add(Comment entity)
+        {
+            DbContext.Add(entity);
+        }
+
+        public void Delete(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment Get(Func<Comment, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Comment> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Comment> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Comment> GetList(Func<Comment, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            DbContext.SaveChanges();
+        }
+
+        public Comment Update(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/TicketWatcherRepository.cs
@@ -0,0 +1,75 @@
+﻿using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.DAL
+{
+    public class TicketWatcherRepository : IRepository<TicketWatcher>
+    {
+        public ApplicationDbContext DbContext { get; set; }
+
+        public TicketWatcherRepository(ApplicationDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public void Add(TicketWatcher entity)
+        {
+            DbContext.TicketWatchers.Add(entity);
+        }
+
+        public void Delete(TicketWatcher entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TicketWatcher Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TicketWatcher Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TicketWatcher Get(Func<TicketWatcher, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TicketWatcher> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TicketWatcher> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<TicketWatcher> GetList(Func<TicketWatcher, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            DbContext.SaveChanges();
+        }
+
+        public TicketWatcher Update(TicketWatcher entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
